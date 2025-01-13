1b8897719a89839bedfd43f52a450b0920412d30
yongfire38 <yongfire38@gmail.com>
2025-01-13 18:13:26 +0900

    Fix: BoardVO / BBSDTO 간 불일치하여 발생하는 오류 수정

--- src/main/java/egovframework/com/ext/ops/service/BoardVO.java
+++ src/main/java/egovframework/com/ext/ops/service/BoardVO.java
@@ -2,6 +2,8 @@ package egovframework.com.ext.ops.service;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +11,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BoardVO extends Board implements Serializable {
 	/** 검색시작일 */
     private String searchBgnDe = "";
