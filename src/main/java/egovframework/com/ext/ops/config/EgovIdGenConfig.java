910270efe33159bbe3313458bb7a438eb385ecd4
yongfire38 <yongfire38@gmail.com>
2025-02-19 15:01:50 +0900

    Fix: datasource 설정 제거 (boot의 자동 설정 이용)

--- src/main/java/egovframework/com/ext/ops/config/EgovIdGenConfig.java
+++ src/main/java/egovframework/com/ext/ops/config/EgovIdGenConfig.java
@@ -4,23 +4,32 @@ import javax.sql.DataSource;
 
 import org.egovframe.rte.fdl.idgnr.impl.EgovTableIdGnrServiceImpl;
 import org.egovframe.rte.fdl.idgnr.impl.strategy.EgovIdGnrStrategyImpl;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import egovframework.com.ext.ops.util.EgovIdGnrBuilder;
 
 @Configuration
 public class EgovIdGenConfig {
 	
-	@Autowired
-	DataSource dataSource;
-	
-	@Bean(destroyMethod = "destroy")
-	public EgovTableIdGnrServiceImpl egovSyncIdGnrService() {
-		return new EgovIdGnrBuilder().setDataSource(dataSource).setEgovIdGnrStrategyImpl(new EgovIdGnrStrategyImpl())
-				.setBlockSize(1).setTable("COMTECOPSEQ").setTableName("SYNC_ID").setPreFix("SYNC_").setCipers(15)
-				.setFillChar('0').build();
+	@Bean
+	public EgovIdGnrStrategyImpl mixPrefixSample() {
+		EgovIdGnrStrategyImpl egovIdGnrStrategyImpl = new EgovIdGnrStrategyImpl();
+		egovIdGnrStrategyImpl.setPrefix("SYNC_");
+		egovIdGnrStrategyImpl.setCipers(15);
+		egovIdGnrStrategyImpl.setFillChar('0');
+		return egovIdGnrStrategyImpl;
 	}
 
+	@Bean(destroyMethod="destroy")
+	public EgovTableIdGnrServiceImpl egovIdGnrService(@Qualifier("dataSource") DataSource dataSource) {
+		EgovTableIdGnrServiceImpl egovTableIdGnrServiceImpl = new EgovTableIdGnrServiceImpl();
+		egovTableIdGnrServiceImpl.setDataSource(dataSource);
+		egovTableIdGnrServiceImpl.setStrategy(mixPrefixSample());
+		egovTableIdGnrServiceImpl.setBlockSize(1);
+		egovTableIdGnrServiceImpl.setTable("COMTECOPSEQ");
+		egovTableIdGnrServiceImpl.setTableName("SYNC_ID");
+		return egovTableIdGnrServiceImpl;	
+	}
+	
+	
 }
