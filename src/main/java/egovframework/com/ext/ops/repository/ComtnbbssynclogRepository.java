b1385578fe6f2e29a24dde946da421128754ec03
yongfire38 <yongfire38@gmail.com>
2025-02-19 18:17:29 +0900

    Fix: 미사용 메서드 제거

--- src/main/java/egovframework/com/ext/ops/repository/ComtnbbssynclogRepository.java
+++ src/main/java/egovframework/com/ext/ops/repository/ComtnbbssynclogRepository.java
@@ -1,7 +1,5 @@
 package egovframework.com.ext.ops.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +7,4 @@ import egovframework.com.ext.ops.entity.Comtnbbssynclog;
 
 @Repository
 public interface ComtnbbssynclogRepository extends JpaRepository<Comtnbbssynclog, String> {
-	// SYNC_STTUS_CODE가 'N'또는 'E'인 데이터를 10건 가져오기 (등록 순서대로)
-		List<Comtnbbssynclog> findTop10BySyncSttusCodeInOrderByRegistPnttmAsc(List<String> syncSttusCodes);
 }
